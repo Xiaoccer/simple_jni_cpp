42f0617c509f2c6219b3e08566c8f5fc22102cd4
Xiaoccer <598887962@qq.com>
2024-05-31 20:49:26 +0800

    fix

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ cd $(dirname $0)
 
 (
     cd hello || exit 1
-    rm *.so
+    find ./ -name "*.so" -exec rm -f {} +
     g++ -fPIC  -I"../jni8/include" -Wl,--no-undefined,--version-script="./jni_symbol.lds"  -shared -o  hello.so hello.cc
 )
 
